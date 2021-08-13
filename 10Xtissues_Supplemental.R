853b55c57716d3f60c13cec8b886b4ba8d2a5436
Jacqueline HY Siu <42074014+JHYSiu@users.noreply.github.com>
2021-08-13 11:20:45 +0100

    Update 10Xtissues_Supplemental.R

--- 10Xtissues_Supplemental.R
+++ 10Xtissues_Supplemental.R
@@ -6,18 +6,24 @@ library(randomcoloR)
 color_use <- c("#7B52DD", "#7DE3D4", "#DC694C", "#77E595", "#E4DB61", "#D78AD6", "#CFDFDE", "#E0E2AC", "#9B9C59", "#778BD5", "#95EA4F", "#D3B6DB", "#75ABC5", "#DA4BA0", "#D49794")
 
 #Directories
-dir <- file.path("C:", "Users", "jacqu", "OneDrive - University Of Cambridge", "PhD", "10X", "R_analysis", "output")
-out_dir <- file.path("C:", "Users", "jacqu", "Dropbox", "NI Submission 2021", "figure images", "Supplemental")
+dir <- file.path("output")
+out_dir <- file.path("figure images", "Supplemental")
 
-#### Fig SX (Integration donor)
+#### Fig S2D (Integration donor)
 DefaultAssay(donor.integrated) <- "integrated"
 
-plot_umapdonor <- DimPlot(donor.integrated, group.by = "donor", pt.size = 0.3) +
+plot_umapdonor <- DimPlot(donor.integrated, group.by = "donor", pt.size = 0.5) +
   theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank())
 file_name <- paste0("UMAP_origident.pdf")
 ggsave(filename = file.path(out_dir, file_name), plot = plot_umapdonor, units = c("cm"))
 
-#### Fig SX (DE genes per subset)
+plot_umapdonor <- DimPlot(donor.integrated, group.by = "donor", pt.size = 0.7, shape.by = "tissue") +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank(), legend.position = "none")
+file_name <- paste0("UMAP_origident_tissuebyshape.pdf")
+ggsave(filename = file.path(out_dir, file_name), plot = plot_umapdonor, units = c("cm"))
+
+
+#### Tables (DE genes per subset)
 idents_list <- levels(donor.integrated$subset_names)
 
 de_pos_list <- {}
@@ -48,7 +54,17 @@ write.csv(de_pos_summary, file=file.path(out_dir, file_name), col.names = F)
 file_name <- paste0("neg_degenes_summary.csv")
 write.csv(de_neg_summary, file=file.path(out_dir, file_name), col.names = F)
 
-#### Fig SX (DN differences)
+
+for (i in 1:length(idents_list)){
+  ident1 <- idents_list[i]
+  
+  condition.diffgenes <- FindMarkers(donor.integrated, ident.1 = ident1, only.pos = TRUE)
+  
+  file_name <- paste0("DEG_", ident1, ".csv")
+  write.csv(condition.diffgenes, file=file.path(out_dir, file_name), col.names = F)
+}
+
+#### Fig S2F (DN differences)
 DefaultAssay(donor.integrated) <- "RNA"
 
 VlnPlot_2 <- function(object, features, idents, cols, pt.size, assay, xlab) {
@@ -68,7 +84,8 @@ VlnPlot_2 <- function(object, features, idents, cols, pt.size, assay, xlab) {
   # https://github.com/satijalab/seurat/blob/master/R/plotting.R#L1100
   # ncol argument adapted from Josh O'Brien
   # https://stackoverflow.com/questions/10706753/how-do-i-arrange-a-variable-list-of-plots-using-grid-arrange
-  cowplot::plot_grid(plotlist = p, ncol = ceiling(sqrt(length(features))))
+  #cowplot::plot_grid(plotlist = p, ncol = ceiling(sqrt(length(features))))
+  cowplot::plot_grid(plotlist = p, ncol = 5)
 }
 
 dn_markers <- c("CXCR5", "ITGAX", "CR2", "CD19", "CD38", "CD24", "CD22", "CD69", "CD86", "HLA-DRA")
@@ -78,7 +95,7 @@ dn_topgenes <- VlnPlot_2(donor.integrated,
                          idents = c("DN-A", "DN-B"), cols = c("#CFDFDE", "#D78AD6"), pt.size = 0.2, assay = "RNA", xlab = " ")
 
 file_name <- paste0("dn_vlnplot_select.pdf")
-ggsave(filename = file.path(out_dir, file_name), plot = dn_topgenes, width = 8, height = 8)
+ggsave(filename = file.path(out_dir, file_name), plot = dn_topgenes, width = 12, height = 8)
 
 ## DotPlot for aNAV
 DefaultAssay(donor.integrated) <- "RNA"
